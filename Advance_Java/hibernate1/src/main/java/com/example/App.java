857247cceda24e3987ee321833d86d66bfe87dda
Akhil Rana <akhilrana4104@gmail.com>
2026-02-14 10:05:01 +0530

    Add Folder

---
+++ Advance_Java/hibernate1/src/main/java/com/example/App.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import java.util.Date;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Project Started");
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+
+        Student student = new Student(102, "xyz", "pune");
+
+        Address address = new Address();
+        address.setStreet("JBRoad");
+        address.setCity("Pune");
+        address.setDate(new Date());
+        address.setX(34.8);
+        address.setOpen(true);
+
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.save(student);
+        session.save(address);
+
+        tx.commit();
+
+        session.close();
+        factory.close();
+
+        System.out.println("Data Saved Successfully!");
+    }
+}
